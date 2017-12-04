ae4420864574b42808d9d0e49f903cddd74bfa59
Stefan Kiskovac <stefan.kiskovac@gmail.com>
2017-12-04 17:32:49 +0100

    add: PORTAL_URL and API_URL as build parameters

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ exports.__esModule = true;
 var ejs_1 = require("ejs");
 var fs_1 = require("fs");
 /**
+ * Load data and replace $vars with actual argumets
+ * @param path
+ */
+function load(path) {
+    return JSON.parse(fs_1.readFileSync(path, 'utf8')
+        .replace(/\$PORTAL_URL/g, process.argv[2])
+        .replace(/\$API_URL/g, process.argv[3]));
+}
+/**
  * Render page from layout
  * @param language
  * @param json
@@ -18,5 +27,10 @@ function render(language, json, path) {
         }
     });
 }
-render('cz', JSON.parse(fs_1.readFileSync('./source/_data/content_cz.json', 'utf8')), './themes/sukl-pristupy-theme/source/index.html');
-render('en', JSON.parse(fs_1.readFileSync('./source/_data/content_en.json', 'utf8')), './themes/sukl-pristupy-theme/source/index_en.html');
+if (process.argv.length < 4) {
+    console.error('Needs at least 2 arguments: PORTAL_URL API_URL');
+    console.dir(process.argv);
+    process.exit(1);
+}
+render('cz', load('./source/_data/content_cz.json'), './themes/sukl-pristupy-theme/source/index.html');
+render('en', load('./source/_data/content_en.json'), './themes/sukl-pristupy-theme/source/index_en.html');
--- index.ts
+++ index.ts
@@ -2,12 +2,23 @@ import { renderFile } from 'ejs';
 import { readFileSync, writeFileSync } from 'fs';
 
 /**
+ * Load data and replace $vars with actual argumets
+ * @param path 
+ */
+function load(path: string) {
+    return JSON.parse(readFileSync(path, 'utf8')
+        .replace(/\$PORTAL_URL/g, process.argv[2])
+        .replace(/\$API_URL/g, process.argv[3])
+    );
+}
+
+/**
  * Render page from layout
  * @param language 
  * @param json 
  * @param path 
  */
-function render(language: string, json:{}, path:string){
+function render(language: string, json: {}, path: string) {
     renderFile('./themes/sukl-pristupy-theme/layout/layout.ejs', { language, json }, (err, str) => {
         if (err) {
             console.error(err)
@@ -16,6 +27,10 @@ function render(language: string, json:{}, path:string){
         }
     })
 }
-
-render('cz', JSON.parse(readFileSync('./source/_data/content_cz.json', 'utf8')),'./themes/sukl-pristupy-theme/source/index.html');
-render('en', JSON.parse(readFileSync('./source/_data/content_en.json', 'utf8')),'./themes/sukl-pristupy-theme/source/index_en.html');
+if (process.argv.length < 4) {
+    console.error('Needs at least 2 arguments: PORTAL_URL API_URL')
+    console.dir(process.argv);
+    process.exit(1);
+}
+render('cz', load('./source/_data/content_cz.json'), './themes/sukl-pristupy-theme/source/index.html');
+render('en', load('./source/_data/content_en.json'), './themes/sukl-pristupy-theme/source/index_en.html');
